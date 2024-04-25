e6a073df6086d668448e462ae0714cd49971f870
임성우 <seongwoorang@naver.com>
2024-04-25 19:59:03 +0900

    [ui-component] - Add color

---
+++ ui-components/src/main/java/com/ownsnoretracker/ui_components/values/Colors.kt
@@ -0,0 +1,24 @@
+package com.ownsnoretracker.ui_components.values
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+val mainTheme = Color(0xFF232A39)
+val transparentBrush = Brush.linearGradient(
+    colors = listOf(Color.Transparent, Color.Transparent)
+)
+val profileInfoButton = Color(0xFFDFDFDF)
+val grayText = Color(0xFFB6BFCD)
+val grayText2 = Color(0xFFA5AAB2)
+val emblem = Color(0xFFAEB9C8)
+val subTheme = Color(0xFF2E3D53)
+val darkGray = Color(0xFF535353)
+val semiRed = Color(0xFFDA7373)
+val semiBlue = Color(0xFF6CACDF)
+val darkButton = Color(0xFF212A3A)
+val purple= Color(0XFF957FEA)
+val pink= Color(0XFFE56DE2)
+val yellow = Color(0xFFFFC107)
+val metal = Color(0xFFC0C0C0)
+val lightPink = Color(0xFFF8BBD0)
+val beige = Color(0xFFF0EDE5)
